24f10defa2a48b9164f5f433292a63e6bd23d168
alex_xd <alexander123azazninjago@gmail.com>
2023-09-08 11:21:10 -0300

    1

--- app1/views.py
+++ app1/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def vista1(recuest):
+    html="""
+    <h1>Vista uno</h1>
+    """
+    return HttpResponse(html)
